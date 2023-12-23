72dd39bb6218061c71d65bb90ee6ac1c04f149b5
creator54 <hi.creator54@gmail.com>
2023-12-23 15:11:35 +0530

    check

--- termux-ssh-setup.sh
+++ termux-ssh-setup.sh
@@ -1,11 +1,36 @@
 #!/usr/bin/env bash
 
-pkg update -y && pkg upgrade -y
+# Define colors
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-pkg install openssh -y
+# Function to print messages with color
+print_message() {
+	echo -e "${GREEN}[INFO]${NC} $1"
+}
 
-passwd
+# Update and upgrade packages
+print_message "Updating and upgrading packages..."
+pkg update -y
+pkg upgrade -y
 
+# Prompt password for the user
+user_password="jack@1234"
+print_message "Setting up password for the user..."
+echo -e "$user_password\n$user_password" | passwd
+
+# Install openssh without prompts
+print_message "Installing OpenSSH..."
+export DEBIAN_FRONTEND=noninteractive
+echo "Y\nY\nY" | pkg install -y openssh
+
+# Generate SSH host keys
+print_message "Generating SSH host keys..."
 ssh-keygen -A
 
+# Start the SSH server
+print_message "Starting the SSH server..."
 sshd
+
+print_message "User setup is complete."
